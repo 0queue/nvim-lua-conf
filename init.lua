50a6c3dc4fd8f83ac30733e1f37cdbeea39f3e17
Thomas Harris <teh019283@gmail.com>
2021-05-29 13:09:35 +0200

    Basic config and vim-gitgutter

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
 require('plugins')
+
+vim.o.termguicolors = true
+
+vim.wo.number = true
+vim.o.ruler = true
+
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
+vim.bo.autoindent = true
+
+vim.wo.cursorline = true
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,22 @@
+-- You must run this or `PackerSync` whenever you make changes to your plugin configuration
+-- :PackerCompile
+
+-- Only install missing plugins
+-- :PackerInstall
+
+-- Update and install plugins
+-- :PackerUpdate
+
+-- Remove any disabled or unused plugins
+-- :PackerClean
+
+-- Performs `PackerClean` and then `PackerUpdate`
+-- :PackerSync
+
+-- Loads opt plugin immediately
+-- :PackerLoad completion-nvim ale
+
+
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 -- bootstrap packer
@@ -6,7 +25,9 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 vim.cmd 'packadd packer.nvim'
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 
 return require('packer').startup(function ()
   use 'wbthomason/packer.nvim'
+  use 'airblade/vim-gitgutter'
 end)

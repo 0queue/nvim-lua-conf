90774e44b4d1f719fdc38be34775a4f71a27bc0c
Thomas Harris <teh019283@gmail.com>
2021-07-03 13:08:08 +0200

    Terminal keybinds

--- init.lua
+++ init.lua
@@ -30,3 +30,5 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', noremap)
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', noremap)
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', noremap)
 vim.api.nvim_set_keymap('', '<Leader>/', ':nohlsearch<Enter>', noremap)
+
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', noremap)
